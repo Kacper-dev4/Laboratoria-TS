0fabc45be75f9cbfeeb7458f42670dbb3f26d2f1
Kacper Wach <wach.kacper8@gmail.com>
2025-10-28 12:31:36 +0100

    Z labów 3

--- lab3 regulacja przekaźnikowa 2/lab3.m
+++ lab3 regulacja przekaźnikowa 2/lab3.m
@@ -1,16 +1,17 @@
 clear all 
 clc
 
-
-for i=1:10
-a = 2;
-e0 = 4;
+%% zad1
+stop_time = 20;
+for i=1:5:16
+a = 0;
+e0 = i;
 eprim0 = 0;
 w = 0;
-stop_time = 20;
-k1 = i*5;
-k2 = i+3;
-B = 2;
+
+k1 = 1.7;
+k2 = 1;
+B = 1;
 
 out = sim('lab3sim.slx');
 
@@ -20,5 +21,56 @@ eprim = out.eprim.Data;
 figure(1)
 hold on
 plot(e,eprim)
+xlabel('e')
+ylabel('eprim')
 
 end
+
+
+for i=4:2.5:14
+a = 0;
+e0 = 0;
+eprim0 = 0;
+w = -i;
+
+k1 = 1.7;
+k2 = 1;
+B = 1;
+
+out = sim('lab3sim.slx');
+
+e = out.e.Data;
+eprim = out.eprim.Data;
+
+figure(2)
+hold on
+plot(e,eprim)
+xlabel('e')
+ylabel('eprim')
+
+end
+
+
+for i=2:2
+a = 0.05;
+e0 = i;
+eprim0 = 0;
+w = 0;
+
+k1 = 2;
+k2 = 1;
+B = 1;
+
+out = sim('lab3sim.slx');
+
+e = out.e.Data;
+eprim = out.eprim.Data;
+
+figure(3)
+hold on
+plot(e,eprim)
+
+end
+
+
+%% zad2
