98d848fb8d7d8b544d0cf562a69bd54ebf48197a
Kacper Wach <wach.kacper8@gmail.com>
2025-11-01 18:50:04 +0100

    Do zadania 3 czwarte jest schemat

--- lab3 regulacja przekaźnikowa 2/lab3.m
+++ lab3 regulacja przekaźnikowa 2/lab3.m
@@ -1,7 +1,10 @@
 clear all 
 clc
-
+k=1;
+kr=1;
+Td=1;
 %% zad1
+T = 2;
 stop_time = 20;
 for i=1:5:16
 a = 0;
@@ -74,3 +77,71 @@ end
 
 
 %% zad2
+
+T = 2;
+stop_time = 40;
+for i=0:1:4
+a = 2;
+e0 = -5;
+eprim0 = 0;
+w = 0;
+k=1;
+k1 = i;
+k2 = 1;
+B = 3;
+
+out = sim('lab3sim.slx');
+
+e = out.e2.Data;
+eprim = out.eprim2.Data;
+
+figure(4)
+hold on
+plot(e,eprim)
+xlabel('e')
+ylabel('eprim')
+legend('k1 = 0', 'k1 = 1', 'k1 = 2','k1 = 3','k1 = 4')
+
+end
+
+%% Zad3
+
+stop_time = 40;
+%i=1.1:0.01:1.2
+for i=1:5
+a = 1;
+e0 = -6;
+eprim0 = 0;
+w = 0;
+k=1;
+k1 = 2;
+k2 = 1;
+B = 3;
+kr = 1;
+T = 2;
+
+Td = i^3;
+
+out = sim('lab3sim.slx');
+e = out.e3.Data;
+eprim = out.eprim3.Data;
+t = out.tout;
+
+figure(5)
+hold on
+plot(e,eprim);
+xlabel('e')
+ylabel('eprim')
+legend( 'Td = 1', 'Td = 2','Td = 3','Td = 4','Td = 5')
+
+figure(6)
+hold on 
+plot(t,e)
+xlabel('t')
+ylabel('e')
+legend( 'Td = 1', 'Td = 2','Td = 3','Td = 4','Td = 5')
+
+end
+
+%% Zad4 
+
