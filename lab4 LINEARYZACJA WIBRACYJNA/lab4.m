da1d3ca67f96df6ed68f1a330883f3124ea2c0c4
Kacper Wach <wach.kacper8@gmail.com>
2025-11-15 13:35:50 +0100

    Zadanie 4 a

--- lab4 LINEARYZACJA WIBRACYJNA/lab4.m
+++ lab4 LINEARYZACJA WIBRACYJNA/lab4.m
@@ -5,7 +5,8 @@ close all
 
 %% Zad1
 stop_time = 100;
-
+kr = 1;
+w = 1;
 T = 1/5;
 A = 1;
 Ax=10;
@@ -128,9 +129,52 @@ end
 
 
 %% Zad4
-
+stop_time = 20;
 % a)
+% w = 1;
+% kr = 2.3;
+% T = 1/5;
+% A = 1;
+% Ax=10;
+% Tfd = 0.3;
+%  M{1} = [ Tfd 1 ];
+%  M{2} = [ Tfd^2  2*Tfd  1 ];
+%  M{3} = [ Tfd^3  3*Tfd^2  3*Tfd  1 ];
+%  M{4} = [ Tfd^4  4*Tfd^3  6*Tfd^2  4*Tfd  1 ];
+%  M{5} = [ Tfd^5  5*Tfd^4  10*Tfd^3  10*Tfd^2  5*Tfd  1 ];
+% mianownik = M{3};
+% out = sim("lab444.slx");
+% t = out.tout;
+% y1 = out.y4a1.Data;
+% figure(6)
+% plot(t,y1);
+% xlabel('czas, s')
+% ylabel('y(t)')
+% 
+% G1 = tf(kr,mianownik);
+% G1z = feedback(G1,1);
+% figure(7)
+% bodemag(G1z)
+
+% w = 1;
+% kr = 1.3;
+% mianownik = M{2};
+% out = sim("lab444.slx");
+% t = out.tout;
+% y2 = out.y4a2.Data;
+% figure(8)
+% plot(t,y2);
+% xlabel('czas, s')
+% ylabel('y(t)')
+% 
+% G2 = tf(kr,[Tfd^2  2*Tfd  1, 0]);
+% G2z = feedback(G2,1);
+% figure(9)
+% bodemag(G2z)
+
 
 % b)
+
+
 
 % c)
